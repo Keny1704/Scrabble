48e7e0491a89363d7a8414ebb0ce81668c560f4e
Keny1704 <54723387+Keny1704@users.noreply.github.com>
2025-05-25 18:25:14 -0400

    Add files via upload

--- src/scrabble/Scrabble.java
+++ src/scrabble/Scrabble.java
@@ -1,8 +1,9 @@
 package scrabble;
-
+import IGU.Front;
 public class Scrabble {
 
     public static void main(String[] args) {
-        
-    }
+        Front i = new Front();
+        i.setVisible(true);
+     }
 }
